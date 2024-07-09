b19656477cd90a2f635931f87bb1be52e2472692
Poojan Patel <p.poojan1762002@gmail.com>
2024-07-09 08:25:22 +0530

    Truty And Falsy

---
+++ 02_Truthy_And_Falsy.js
@@ -0,0 +1,7 @@
+console.log(Boolean(0));
+console.log(Boolean(-0));
+console.log(Boolean(false));
+console.log(Boolean(""));
+console.log(Boolean(NaN));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
