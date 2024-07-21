c0f651cce3d12c82df3fe0b5859c6282b810b106
Poojan Patel <p.poojan1762002@gmail.com>
2024-07-21 17:53:26 +0530

    Object Freeze & Seal

---
+++ 13_Objectfreeze_&_Objectseal.js
@@ -0,0 +1,49 @@
+const myName = 'Anurag'
+
+const username1 = ''
+const username2 = ''
+
+const user1 = {
+    firstName: 'Akash'
+}
+
+
+// const user2 = {
+//     firstName: 'Adarsh',
+//     'last-Name': 'Singh',
+//     Anurag: 'Developer'
+// }
+
+// console.log(user2.firstName);
+// console.log(user2["last-Name"]);
+// console.log(user2[myName]);
+// console.log(user2["first" + "Name"]);
+
+const user2 = {
+    firstName: 'Adarsh',
+    lastName: 'Singh',
+    pata: {
+        city: 'Bangalore',
+        pinCode: 876876,
+        state: 'Karnataka',
+        moreDetails: {
+            population: 9798897897,
+            area: '787 sq km',
+        }
+    }
+}
+
+delete user2.pata.moreDetails.area;
+// user2.age = 26
+// user2["is-student"] = true
+
+
+//Object.seal()
+Object.seal(user2)
+user2.firstName="Poojan"
+console.log(user2)
+
+// Object.freeze()
+Object.freeze(user1)
+user1.firstName="Poojan"
+console.log(user1)
