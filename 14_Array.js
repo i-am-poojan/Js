fd611c877e5175e36dea4a23f434ec75a770170b
Poojan Patel <p.poojan1762002@gmail.com>
2024-07-22 08:50:53 +0530

    Basic Array

---
+++ 14_Array.js
@@ -0,0 +1,20 @@
+const fruitList=['Mango','Banana']
+// How to change Existing Properties in fruitList
+fruitList[1]='Graphes'
+console.log(fruitList)
+
+// Hot to add Data To fruitList
+//using Index Number
+fruitList[2]="Banana"
+console.log(fruitList)
+
+fruitList[5]="WaterMelon"
+console.log(fruitList)
+
+//using push method
+fruitList.push("Apple")
+console.log(fruitList)
+
+//Delete data of lastIndex
+fruitList.pop()
+console.log(fruitList)
