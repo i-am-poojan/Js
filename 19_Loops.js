3d9c925a684bc088669495361ed42da6a2f4bbad
Poojan Patel <p.poojan1762002@gmail.com>
2024-07-23 21:43:24 +0530

    Loops

---
+++ 19_Loops.js
@@ -0,0 +1,20 @@
+let fruitList=["Apple","Banana","Charry","Mango","Graphes","Oranges"]
+
+//for Loop
+for(let i=0;i<fruitList.length;i++){
+    console.log(`${fruitList[i]}`+" is a best fruit for health.")
+}
+
+//while Loop
+let j=0;
+while(j<fruitList.length){
+    console.log(`${fruitList[j]}`+" is a while loop collection fruit")
+    j++;
+}
+
+//Dowhile Loop
+let k=0;
+do {
+    console.log(`${fruitList[k]}`+" is a dowhile loop collection fruit");
+    k++;
+} while (k<fruitList.length);
