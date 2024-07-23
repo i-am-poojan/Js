c5cc49b4aebe7fb8e30efe17576f20042727b007
Poojan Patel <p.poojan1762002@gmail.com>
2024-07-23 18:21:29 +0530

    Multidimension_Array-&_flat_method

--- 16_Multidimensionarray.js
+++ 16_Multidimensionarray.js
@@ -1,2 +1,16 @@
-const insideArray=[1,2,3,4,["Poojan",'Dev,"Kushal']]
-console.log
+const insideArray=[1,2,3,4,["Poojan",'Dev',"Kushal"]]
+console.log(insideArray)
+console.log(insideArray[4][2])
+
+const tictactoe=[
+    ['x',null,null],
+    [null,null,'0'],
+    ['0',null,'x']
+]
+console.log(tictactoe)
+
+console.log(insideArray.flat())
+console.log(tictactoe.flat())
+
+const multipleInsider=[1,2,3,[4,5,[8,9,7]]]
+console.log(multipleInsider.flat(2))
