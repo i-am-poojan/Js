dceb7a1bdaaba067a9f5a6108c295ed9b0534a7a
Poojan Patel <p.poojan1762002@gmail.com>
2024-07-31 07:27:30 +0530

    SetTimeOut and SetInterval

---
+++ 24_SetTimeOut_SetInterval.js
@@ -0,0 +1,14 @@
+// const stemp1=setTimeout('console.log("hi how are you")',3000)
+// const stemp2=setTimeout(function(){
+//     console.log("i am running after 5sec ")
+// },10000)
+// const stemp3=setTimeout('console.log("i am first")')
+
+// console.log(stemp1)
+// console.log(stemp2)
+// console.log(stemp3)
+// clearTimeout(2)
+
+const stemp4=setInterval('console.log("i am Interval")',5000);
+console.log(stemp4)
+clearInterval(stemp4)
