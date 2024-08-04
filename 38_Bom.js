074687945b0f53040907da8be052cfc0458671b9
Poojan Patel <p.poojan1762002@gmail.com>
2024-08-04 19:03:39 +0530

    Bom(Browser Object Model)

---
+++ 38_Bom.js
@@ -0,0 +1,22 @@
+// you can do it this thing in inspect
+// window.location
+
+// setTimeout(()=>{
+//     location.reload()
+// },1000)
+
+// location.href="https://www.google.com/"
+
+// history
+// history.forward()
+// history.backward()
+// history.go(step)  //here according to step you can forward & backward
+
+// innerHeight
+// outerHeight
+// innerWidth
+
+open('script.js')
+open('https://www.youtube.com')
+
+close()
