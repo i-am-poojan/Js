3c26f2f2e3860e52f665661f76dba07dca59de25
Poojan Patel <p.poojan1762002@gmail.com>
2024-08-07 08:15:14 +0530

    InnerHtml , InnerText & Text content

---
+++ 41_InnerHtml_InnerText_TextContent.js
@@ -0,0 +1,8 @@
+// const innerHtmlTest=document.querySelectorAll('ul li')
+// console.dir(innerHtmlTest.innerHtml)
+
+// const innerTextTest=document.querySelectorAll('ul li')
+// console.dir( innerTextTest.innerText)
+
+// const textContent=document.querySelectorAll('ul li')
+// console.dir( innerTextTest.textContent)
