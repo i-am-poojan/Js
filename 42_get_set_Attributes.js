f0265df1c0f3786504991d66361ebd21baf27c29
Poojan Patel <p.poojan1762002@gmail.com>
2024-08-09 08:33:50 +0530

    Get & Set Attributes

---
+++ 42_get_set_Attributes.js
@@ -0,0 +1,6 @@
+let attributeValue=document.querySelector('[procoder]').attributes.procoder.value
+// console.dir(attributeValue)
+
+let getAttributes=document.querySelector('[procoder]').getAttribute('get')
+
+let setAttributes=document.querySelectorAll('h2')[0].setAttribute('id','setId')
